babe2339e4af6f94e983e214961d819a25bd2e32
Elif DURAN <elif_duran@eskisehir.edu.tr>
2022-10-25 02:09:46 +0300

    CRUD operations was completed

---
+++ Owner.API/Controllers/OwnerController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Owner.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]s")]
+    public class OwnerController : ControllerBase
+    {
+        [Route("AllOwners")]
+        [HttpGet]
+        public List<Owner.API.Modal.Owner> GetAllOwners()
+        {
+            List<Owner.API.Modal.Owner> ownerList = new List<Owner.API.Modal.Owner>();
+            DateTime today = DateTime.Now;
+            Owner.API.Modal.Owner owner1 = new Owner.API.Modal.Owner()
+            {
+                Id = 1,
+                Name = "Elif",
+                Surname = "Duran",
+                Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                Type = "Type1",
+                Date = today                
+            };
+            ownerList.Add(owner1);
+
+            Owner.API.Modal.Owner owner2 = new Owner.API.Modal.Owner()
+            {
+                Id = 2,
+                Name = "Sena",
+                Surname = "Duran",
+                Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                Type = "Type2",
+                Date = today                
+            };
+
+            ownerList.Add(owner2);
+
+            Owner.API.Modal.Owner owner3 = new Owner.API.Modal.Owner()
+            {
+                Id = 3,
+                Name = "Çağlar",
+                Surname = "Duran",
+                Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                Type = "Type3",
+                Date = today
+            };
+
+            ownerList.Add(owner3);
+            return ownerList;
+        }
+
+        [Route("DeleteOwner/{id}")]
+        [HttpDelete]
+        public bool DeleteOwner(int ownerId)
+        {
+            List<Owner.API.Modal.Owner> ownerList = new List<Owner.API.Modal.Owner>();
+            DateTime today = DateTime.Now;
+            Owner.API.Modal.Owner owner1 = new Owner.API.Modal.Owner()
+            {
+                Id = 1,
+                Name = "Elif",
+                Surname = "Duran",
+                Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                Type = "Type1",
+                Date = today
+            };
+
+            ownerList.Add(owner1);
+
+            Owner.API.Modal.Owner owner2 = new Owner.API.Modal.Owner()
+            {
+                Id = 2,
+                Name = "Sena",
+                Surname = "Duran",
+                Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                Type = "Type2",
+                Date = today
+            };
+
+            ownerList.Add(owner2);
+
+            var selectedOwner = ownerList.FirstOrDefault(x=> x.Id == ownerId);
+
+            return ownerList.Remove(selectedOwner);           
+           
+        }
+
+        [Route("UpdateOwner")]
+        [HttpPut]
+        public Owner.API.Modal.Owner UpdateOwner(Owner.API.Modal.Owner owner)
+        {
+            var allOwners = GetAllOwners();
+            var updatedOwner = allOwners.Find(x => x.Id == owner.Id);
+            updatedOwner.Id = owner.Id;
+            updatedOwner.Name = owner.Name;
+            updatedOwner.Surname = owner.Surname;
+            updatedOwner.Description = owner.Description;
+            updatedOwner.Date = owner.Date;
+            updatedOwner.Type = owner.Type;
+
+            return updatedOwner;
+        }
+
+        [ConsumesAttribute("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Modal.Owner))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(Modal.Owner))]
+        [Route("CheckDescripton")]
+        [HttpPost]
+        public IActionResult CheckOwnerDescription([FromBody]string keyword)
+        {
+            var owners = GetAllOwners();
+            var lowerCaseWord = keyword.ToLower();
+            var isKeywordFound = owners.Any(x => x.Description.Contains(lowerCaseWord));
+            if (isKeywordFound)
+            {
+                return Ok("Keyword found in description");
+            }
+            else
+            {
+                return NotFound("Keyword does not found in description!");
+            }
+        }
+    }
+}
---
+++ Owner.API/Modal/Owner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Owner.API.Modal
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public string Description { get; set; }
+        public int MyProperty { get; set; }
+        public string Type { get; set; }
+
+    }
+}
